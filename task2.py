c36aff2a52cebd427587dfdf5749d05dd774a201
ujeong1 <70083011+ujeong1@users.noreply.github.com>
2020-10-21 00:08:21 -0700

    Update task2.py
    Edit Distance + Dynamic Time Warping

--- task2.py
+++ task2.py
@@ -13,7 +13,7 @@ def dot_similarity(tfidfs , key):
                                                                             
     return related_docs_indices[1:11]                             
 
-def edit_distance(str1, str2, m, n):
+def edit_distance(sensor1, sensor2, m, n):
     # Create a table to store results of subproblems 
     dp = [[0 for x in range(n + 1)] for x in range(m + 1)] 
   
@@ -33,7 +33,7 @@ def edit_distance(str1, str2, m, n):
   
             # If last characters are same, ignore last char 
             # and recur for remaining string 
-            elif str1[i-1] == str2[j-1]: 
+            elif sensor1[i-1] == sensor2[j-1]: 
                 dp[i][j] = dp[i-1][j-1] 
   
             # If last character are different, consider all 
@@ -44,20 +44,27 @@ def edit_distance(str1, str2, m, n):
                                    dp[i-1][j-1])    # Replace 
   
     return dp[m][n] 
+def dynamic_time_warping(sensor1, sensor2, m, n):
+    dp = [[0 for x in range(n+1)] for x in range(m+1)] 
+    
+    for i in range(1, m+1): 
+        for j in range(1, n+1): 
+            dp[i][j] = abs(int(sensor1[i-1] - sensor2[j-1]))
+             
+            if i == 1 and j == 1:
+                continue
+            elif i == 1 and j != 1:
+                dp[i][j] += dp[i][j-1] 
+            elif i != 1 and j == 1:
+                dp[i][j] += dp[i-1][j]
+            else:
+                dp[i][j] += min(dp[i][j-1],        # Insert 
+                                dp[i-1][j],        # Remove 
+                                dp[i-1][j-1])    # Replace 
+            
+    return dp[m][n] 
 
-'''
-    #win is quantized symbols of sensor
-    if m == 0:
-        return n
-    if n == 0:
-        return m
-    if sensor1[m-1] == sensor2[n-1] :
-        return edit_distance(sensor1, sensor2, m-1, n-1)
-
-    return 1+min(edit_distance(sensor1, sensor2, m, n-1),
-                 edit_distance(sensor1, sensor2, m-1, n),
-                 edit_distance(sensor1, sensor2, m-1, n-1))
-'''                                                                            
+# This code is contributed by Bhavya Jain 
 directory = "./Data/Z/"
 gestures = []
 for filename in os.listdir(directory):
@@ -89,20 +96,55 @@ for filename in os.listdir(directory):
         word = [re.findall(r'\d+',word)[0] for word in sensor.split(" - ")[1].split(",")]
         matrix_sym_quant.append(word)
     gestures.append(matrix_sym_quant)
+print("Enter 6(ED) or 7(DTW) : ")
+user_option = int(input())
+if user_option == 6 :
+    print("Enter gesture name to compare : ")
+    key_idx = int(input())
+    print("Enter number of gesture to retrieve : ")
+    top_K = int(input())
+    key_gesture = gestures[key_idx]
+    #compare every sensor(which contains quntized_window_vector) with gesture file in DB
+    cost=[0]*len(gestures)
+    for i, gesture in enumerate(gestures) :
+        if gesture == key_gesture :
+            continue
+        for j, sensor in enumerate(gesture) :
+            n = len(sensor)
+            m = len(key_gesture[j])
+            cost[i] += edit_distance(key_gesture[j], sensor, m, n)
+    gesture_id = [cost.index(max_cost) for max_cost in heapq.nlargest(top_K, cost)]
+    print("Most similar gestures (Edit distance) :",gesture_id)
 
-print("Enter gesture name to compare : ")
-key_idx = int(input())
-print("Enter number of gesture to retrieve : ")
-top_K = int(input())
-key_gesture = gestures[key_idx]
-#compare every sensor(which contains quntized_window_vector) with gesture file in DB
-cost=[0]*len(gestures)
-for i, gesture in enumerate(gestures) :
-    if gesture == key_gesture :
-        continue
-    for j, sensor in enumerate(gesture) :
-        n = len(sensor)
-        m = len(key_gesture[j])
-        cost[i] += edit_distance(key_gesture[j], sensor, m, n)
-gesture_id = [cost.index(max_cost) for max_cost in heapq.nlargest(top_K, cost)]
-print("Most similar gestures (Edit distance) :",gesture_id)
+elif user_option == 7 :
+    print("Enter gesture name to compare : ")                                   
+    key_idx = int(input())                                                      
+    print("Enter number of gesture to retrieve : ")                             
+    top_K = int(input())                                                        
+
+    avg_gestures = []
+    for gesture in gestures:
+        avg_gesture=[]
+        for sensor in gesture :
+            avg_sensor=[]
+            for window in sensor :
+                avg_win = 0
+                for word in window :
+                    avg_win += int(word)/3
+                avg_sensor.append(int(avg_win))
+            avg_gesture.append(avg_sensor)
+        avg_gestures.append(avg_gesture)
+
+    key_gesture = avg_gestures[key_idx]
+    cost=[0]*len(avg_gestures)
+    for i, gesture in enumerate(avg_gestures) :                                     
+        if gesture == key_gesture :                                             
+            continue                                                            
+        for j, sensor in enumerate(gesture) :                                   
+            n = len(sensor)                                                     
+            m = len(key_gesture[j])                                             
+            cost[i] += dynamic_time_warping(key_gesture[j], sensor, m, n)    
+    gesture_id = [cost.index(max_cost) for max_cost in heapq.nlargest(top_K, cost)]
+    print("Most similar gestures (Dynamic Time Warping) :",gesture_id)
+else:
+    print("No such option (Please enter integer). Program ended")
