80aadd2bf4c2fa09683981014c2630ff23ce4bd6
Alexandra Szilagy <alexszilagy@asu.edu>
2020-10-24 12:41:40 -0700

    task 1 now creates pickle files
    SVD and MNF are implemented, but not tested fully yet. LDA crashes

--- task1.py
+++ task1.py
@@ -1,7 +1,12 @@
 #Task 1
 import glob
 import pandas as pd
+import numpy as np
+import pickle
 from sklearn.decomposition import PCA
+from sklearn.decomposition import TruncatedSVD
+from sklearn.decomposition import NMF
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis as LDA
 
 #unnecessary lines if you are using anaconda or another 
 print("Please enter the following inputs as the same values you used for task 0: ")
@@ -24,13 +29,48 @@ def PCAsetup(wordMat, k):
     
     return topK
 
-def SVD(k):
+def SVDsetup(wordMat, k):
+    #calculate SVD
+    svd =  TruncatedSVD(k)
+    sv = svd.fit_transform(wordMat)
+    topK = pd.DataFrame(data = sv)
     return topK
 
-def NMF(k):
+def NMFsetup(wordMat, k):
+    #calculate NMF
+    nmf = NMF(k)
+    nm = nmf.fit_transform(wordMat)
+    topK = pd.DataFrame(data = nm)
     return topK
 
-def LDA(k):
+def LDAsetup(wordMat, k):
+    #calculate LDA
+    """startI = "1"
+    for y in range(1, w):
+        startI = startI + "1"
+    startI = int(startI)
+    
+    labels = []
+    numWords = startI * (2*r) - startI
+    for i in range(0, numWords + 1):
+        labels.append(str(startI + i))
+    #print(labels)
+    """
+    
+    labels = []
+    for i in range(0, len(wordMat)):
+        labels.append(i % 3 )
+    print(np.shape(labels))
+    
+    print(np.shape(wordMat))
+    #labels = np.transpose(wordMat)[1][:]
+    #print(np.transpose(labels))
+    print(np.unique(labels))
+    
+    lda = LDA(k)
+    ld = lda.fit_transform(wordMat, labels)
+    topk = pd.DataFrame(data = ld)
+    
     return topK
 
 
@@ -90,8 +130,69 @@ def makeMat(vectModel, axis):
            
         return Xmat
     
-    elif vectModel == "idf":
-        return wordMat
+    elif vectModel == "tfidf":
+        Xmat = []
+        for file in glob.glob(directory + axis + "/tfidf_vectors_*.txt"):
+            #read tf file
+            f = open(file, "r")
+            tf_vectors = f.readlines()
+        
+            gestWords = []
+            tfVals = []
+        
+            #split the line into the word and tf value
+            for line in tf_vectors:
+                noDash = line.split("-")
+                tf_val = noDash[1]
+                word = noDash[0].split(",")
+                word[2] = word[2].replace(")","")
+                word[2] = word[2].replace("'", "")
+                word[2] = word[2].replace(" ", "")
+                tf_val = tf_val.replace("\n", "")
+                gestWords.append(word[2])
+                tfVals.append(tf_val)
+           
+        
+            index = 0
+            startI = "1"
+            for y in range(1, w):
+                startI = startI + "1"
+            startI = int(startI)
+        
+            #create dictionary with every word
+            numWords = startI * (2*r) - startI
+            wordMat = []
+    
+            for i in range(0, numWords + 1):
+                wordMat.append(0)
+            
+            # put tf values into matrix where column = word
+            for x in gestWords:
+                xint = int(x)
+            
+                #if wordMat[xint - startI] == 0:
+                wordMat[xint - startI] = wordMat[xint - startI] + float(tfVals[index])
+                index = index + 1
+                
+            
+            for iterate in range(0, len(wordMat)):
+                wordMat[iterate] = wordMat[iterate] / 20
+
+            Xmat.append(wordMat)
+            f.close()
+           
+        return Xmat
+    
+def createdictofComponents(topk, k):
+    
+    
+    print(topk[0][1])
+    for i in range(0, k):
+        word = "w" + str(k)
+        for j in range(0, len(topk)):
+            topk[i][j] = (word, topk[j][i])
+            
+    print(topk)
     
 def task1(gestfiles, vectModel, useOp, k):
     
@@ -102,12 +203,37 @@ def task1(gestfiles, vectModel, useOp, k):
         print("\nPCA for " + gestfiles + " gesture files:\n")
         print(topk)
         
+        original_df = pd.DataFrame(topk)
+        original_df.to_pickle("./PCA_" + gestfiles + ".pkl")
+        
+        #dictofComponents = createdictofComponents(topk, k)
+        
     elif useOp == "SVD":
-        topk = SVD(k)
+        wordMat = makeMat(vectModel, gestfiles)
+        topk = SVDsetup(wordMat, k)
+        print("\nSVD for " + gestfiles + " gesture files:\n")
+        print(topk)
+        
+        original_df = pd.DataFrame(topk)
+        original_df.to_pickle("./SVD_" + gestfiles + ".pkl")
+        
     elif useOp == "NMF":
-        topk = NMF(k)
+        wordMat = makeMat(vectModel, gestfiles)
+        topk = NMFsetup(wordMat, k)
+        print("\nNMF for " + gestfiles + " gesture files:\n")
+        print(topk)
+        
+        original_df = pd.DataFrame(topk)
+        original_df.to_pickle("./NMF_" + gestfiles + ".pkl")
+        
     elif useOp == "LDA":
-        topk = LDA(k)
+        wordMat = makeMat(vectModel, gestfiles)
+        topk = LDAsetup(wordMat, k)
+        print("\nLDA for " + gestfiles + " gesture files:\n")
+        print(topk)
+        
+        original_df = pd.DataFrame(topk)
+        original_df.to_pickle("./LDA_" + gestfiles + ".pkl")
 
 
 gestfiles = input("Enter the folder that you want analyzed: ")
@@ -116,6 +242,7 @@ k = input("Enter k: ")
 useOp = input("Enter the analysis you would like to use: ")
 k = int(k)
 task1(gestfiles, vectModel, useOp, k)
+
 
 #sample output: 
 #Enter the folder that you want analyzed: X
